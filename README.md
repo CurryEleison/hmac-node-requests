3e8b136fff780bd5c7178ac5a1ace09ca62bd2a2
tpe <thomas@simplesite.com>
2022-04-15 07:05:07 +0200

    Initial revision

---
+++ README.md
@@ -0,0 +1,36 @@
+# HMAC Request Tester
+
+Use to run requests against services secured by HMAC auth in the style of 
+https://bitoftech.net/2014/12/15/secure-asp-net-web-api-using-api-key-authentication-hmac-authentication/ .
+Code is from https://stackoverflow.com/questions/49495333/nodejs-equivalent-of-c-sharp-code-for-hmac-sha256-authorization .
+
+## Installation
+
+Do an `npm install` to get dependencies. If you use a `NODE_ENV` then make a copy
+of `config/default.json` to `config/$NODE_ENV.json` and edit it, to add the right
+AppId and Api/Secret key and a url.
+
+## Usage
+
+When configured, to make a GET against the default url do
+
+```sh
+node getreq.js
+```
+
+If you want to do a GET request against a specific url do
+```sh
+node getreq.js https://apiserver.wherever.com/api/path/to/resource
+```
+
+For a POST request do
+
+```sh
+node postreq.js
+```
+
+To use a file as POST body against a specific resource:
+
+```sh
+node postreq.js https://apiserver.wherever.com/api/post/resource yourfile.json
+```
---
+++ getreq.js
@@ -0,0 +1,34 @@
+// Borrowd from https://stackoverflow.com/questions/49495333/nodejs-equivalent-of-c-sharp-code-for-hmac-sha256-authorization
+import fetch from 'node-fetch';
+import { v4 as uuidv4 } from 'uuid';
+import {createHmac} from 'crypto';
+import nodeConfig from 'config';
+
+const args = process.argv.slice(2);
+const url = args[0] ? args[0] : nodeConfig.get("req.defaultUrl");
+
+const appId = nodeConfig.get("auth.appId");
+const secretKey = nodeConfig.get("auth.secretKey")
+
+const requestUri = encodeURIComponent(url).toLowerCase();
+const requestHttpMethod = "GET"; //should be dynamic
+
+const requestTimeStamp = Math.floor(new Date().getTime() / 1000).toString();
+
+const nonce = uuidv4().replace(/-/g, '');
+const signatureRawData = appId + requestHttpMethod + requestUri + requestTimeStamp + nonce;
+
+const key = Buffer.from(secretKey, 'base64');
+const requestSignatureBase64String = createHmac('sha256', key).update(signatureRawData, 'utf8').digest('base64');
+
+const hitExternalAPI = async url => {
+  try {
+    const res = await fetch(url, { method: requestHttpMethod, headers: { "Authorization": "amx "+appId+":"+requestSignatureBase64String+":"+nonce+":"+requestTimeStamp } })
+    .then(res => {
+      console.log(res.ok);
+    });
+  } catch (error) {
+    console.log("Error",error);
+  }
+};
+hitExternalAPI(url);
